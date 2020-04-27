fcee178648330d065eaa9f209d3d1adbc3550b24
lorens-osman <lorans.ev@gmail.com>
2020-04-27 08:08:43 +0300

    v1.0

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+ 
+
+  
+  
+  var typedo = new Typed('.element', {
+    strings: ["px <i class='fa fa-angle-right'></i> rem <i class='fa fa-check-circle-o'></i>", "Direction sensitive <i class='fa fa-check-circle-o'></i>"],
+    typeSpeed: 90,
+    showCursor: true,
+    loop:true
+   
+  });
